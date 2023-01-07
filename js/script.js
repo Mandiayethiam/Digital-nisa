58ad379db8f0a8417466eabb64ffabfceed8ef04
Mandiaye thiam <mandiayethiam1999@gmail.com>
2023-01-07 12:28:07 +0000

    troisieme

--- js/script.js
+++ js/script.js
@@ -1,91 +1,22 @@
-const headerBg= () => {
-  const header = document.querySelector(".js-header");
-  
-  window.addEventListener("scroll",function(){
-      if(this.scrollY> 0){
-          header.classList.add("bg-reveal")
-      }else{
-          header.classList.remove("bg-reveal")
-      }
-  });
-  }
-  headerBg();
-  /* Partie TEMOIGNAGE DEBUT */
-  var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 1,
-      spaceBetween: 30,
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
-    
-  /* Partie TEMOIGNAGE FIN */
-  /* Partie DOMAINE DEBUT */
-  let filter_btns = document.querySelectorAll('.filter-btn');
-  let projects = document.querySelectorAll('.filter-project-item');
-  
-  filter_btns.forEach((filter_btn) => {
-    filter_btn.addEventListener('click', () => {
-      filter_btns.forEach((filter_btn) => {
-        filter_btn.classList.remove('active');
-      });
-      filter_btn.classList.add('active');
-      let selected = filter_btn.getAttribute('data-project');
-      projects.forEach((project, evenement) => {
-        let project_length = projects.length;
-        if (
-          project.getAttribute('data-project') === selected ||
-          selected === 'all'
-        ) {
-          if (project.style.transition) {
-            project.style.transition = '';
-          } else {
-            project.style.transition = `width 0.5s ease ${
-              index / project_length + 0.6
-            }s,height 0.5s ease ${evenement / project_length + 0.6}s`;
+$(document).ready(function(){
+  $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay:true,
+      autoplaySpeed:1500,
+      arrows:false,
+      dots:false,
+      pauseOnHover: false,
+      responsive: [{
+          breakpoint: 768,
+          setting:{
+              slidesToShow:4
           }
-          project.classList.add('show');
-          project.classList.remove('hide');
-        } else {
-          project.style.transition = `width 0.5s ease ${
-            index / project_length + 0.6
-          }s,height 0.5s ease ${index / project_length + 0.6}s`;
-          project.classList.remove('show');
-          project.classList.add('hide');
-        }
-      });
-    });
+      },{
+          breakpoint: 520,
+          setting: {
+              slidesToShow: 3
+          }
+      }]
   });
-  /* Partie DOMAINE FIN */
-  
-  
-  
-  
-  
-  
-  let slides = document.querySelectorAll('.slide-container');
-      let index = 0;
-      
-      function next(){
-        slides[index].classList.remove('active');
-        index = (index + 1) % slides.length; 
-        slides[index].classList.add('active');
-      }
-  
-      function prev(){
-        slides[index].classList.remove('active');
-        index = (index - 1 + slides.length) % slides.length; 
-        slides[index].classList.add('active');
-      }
-  
-      setInterval(next, 7000);  
-  
-  
-  
-  
+});
